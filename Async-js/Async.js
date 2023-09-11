385b77ebf49ad6b9aa42ae231b1799b62273e14b
Shadow boi <anasimamshaikh@gmail.com>
2023-09-11 11:45:44 +0530

    Async Practice

---
+++ Async-js/Async.js
@@ -0,0 +1,63 @@
+// console.log("I");
+// console.log("eat");
+// console.log("icecream");
+
+// setTimeout(()=>{
+//     console.log("I also like to eat milkshakes");
+// },2000)
+
+let stocks={
+    Fruits:["Apple", "banana", "mango", "strawberry"],
+    liquid:["water","ice"],
+    holder:["cone","cup","stick"],
+    toppings:["chocolate","sprinkles"]
+}
+let is_shop_open = true;
+
+function time(ms) {
+
+   return new Promise( (resolve, reject) => {
+
+      if(is_shop_open){
+         setTimeout(resolve,ms);
+      }
+
+      else{
+         reject(console.log("Shop is closed"))
+      }
+    });
+}
+
+async function kitchen(){
+    try{
+	await time(2000)
+	console.log(`${stocks.Fruits[0]} was selected`)
+
+	await time(0)
+	console.log("production has started")
+
+	await time(2000)
+	console.log("fruit has been chopped")
+
+	await time(1000)
+	console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`)
+
+	await time(1000)
+	console.log("start the machine")
+
+	await time(2000)
+	console.log(`ice cream placed on ${stocks.holder[1]}`)
+
+	await time(3000)
+	console.log(`${stocks.toppings[0]} as toppings`)
+
+	await time(2000)
+	console.log("Serve Ice Cream")
+    }
+
+    catch(error){
+	 console.log("customer left")
+    }
+}
+
+kitchen();
